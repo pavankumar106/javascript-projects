c4d6d2b31efb71ae2ba9694902f766e226b954a0
Pavankumar Guttedar <pavankumar494748@gmail.com>
2023-07-30 23:59:56 +0530

    hex-color coder

---
+++ hex-color-code/script.js
@@ -0,0 +1,27 @@
+function changeColor() {
+  var hex_numbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+  var hexCode = "";
+  for (var i = 0; i < 6; i++) {
+    var random_index = Math.floor(Math.random() * hex_numbers.length);
+    hexCode += hex_numbers[random_index];
+  }
+  document.getElementById("hex-code").innerHTML = hexCode;
+  document.getElementsByTagName("body")[0].style.background = "#" + hexCode;
+}
