ca2aaf465b9b0a44efea02db80ec983645da06ce
Pavankumar Guttedar <pavankumar494748@gmail.com>
2023-08-01 16:33:01 +0530

    pass the message

---
+++ message/script.js
@@ -0,0 +1,10 @@
+const messageInput = document.getElementById("message-input");
+
+messageInput.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") getMessage();
+});
+
+function getMessage() {
+  document.getElementById("message-output").innerHTML = messageInput.value;
+  messageInput.value = "";
+}
