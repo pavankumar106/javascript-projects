dea70c9fe48b98e640350f197f78c9205c8649c3
Pavankumar Guttedar <pavankumar494748@gmail.com>
2023-08-03 22:57:25 +0530

    dice roll animation

--- dice-roll/script.js
+++ dice-roll/script.js
@@ -3,11 +3,10 @@ function shuffle() {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
   var diceSrc = "./images/" + diceNumber + ".png";
   img.setAttribute("src", diceSrc);
-    console.log(diceNumber);
+  console.log(diceNumber);
 }
-// function animate(){
-//     setTimeout(shuffle,500){
-//         const img=document.getElementById("img");
-//         img.setAttribute("src", "dice-animate.GIF");
-//     }
-// }
+function anim() {
+  setTimeout(shuffle, 500);
+  const img = document.getElementById("img");
+  img.setAttribute("src", "./images/dice-animate.gif");
+}
