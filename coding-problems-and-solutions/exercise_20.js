263916adc29fc561a7c5f2aec7b3bcb21c77b8d1
Pavankumar Guttedar <pavankumar494748@gmail.com>
2023-08-08 21:52:00 +0530

    arrange string in alphabetical order

---
+++ coding-problems-and-solutions/exercise_20.js
@@ -0,0 +1,6 @@
+// arrange in alphabetical order
+function sortAlphabetic(str) {
+  return str.split("").sort().join("");
+}
+console.log(sortAlphabetic("gbvdhsgjshdjs"));
+console.log(sortAlphabetic("gbvdhs"));
