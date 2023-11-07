9bf3d3778e00a461590b9411a1fa20c762fdbc89
Pavankumar Guttedar <74823464+pavankumar106@users.noreply.github.com>
2023-11-07 23:26:51 +0530

    pattern print

---
+++ coding-problems-and-solutions/exercise_30.js
@@ -0,0 +1,16 @@
+// pattern printing 
+// right angle tringle
+
+for(let i=1 ; i<=3 ; i++){
+    for(let j=1 ; i<=i; j++){
+        document.write("*  ");
+    }
+    document.write("<br>");
+}
+// OUTPUT
+//  *
+//  * *
+//  * * *
+//  * * * *
+//  * * * * *
+//  * * * * * *
