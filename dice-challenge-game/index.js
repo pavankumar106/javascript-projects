0f07265c645c2db347c9eeb65f67d1307d165785
Pavankumar Guttedar <pavankumar494748@gmail.com>
2024-02-16 17:52:27 +0530

    add button

--- dice-challenge-game/index.js
+++ dice-challenge-game/index.js
@@ -4,6 +4,12 @@ var randomImageSource = "images/" + randomDiceImage;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  location.reload();
+});
+
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
