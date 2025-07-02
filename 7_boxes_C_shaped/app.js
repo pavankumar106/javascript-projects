2c50391029cfcdf036e2231a4b96552fd36b6f82
Pavankumar Guttedar <74823464+pavankumar106@users.noreply.github.com>
2025-07-02 16:02:27 +0530

    Create app.js

---
+++ 7_boxes_C_shaped/app.js
@@ -0,0 +1,35 @@
+const boxes = document.getElementsByClassName("box");
+
+const seq = [];
+
+const checkAllBoxesAreRed = () => {
+  const boxNumbers = [1, 2, 3, 4, 5, 6, 7];
+  // Extract numbers from seq (which contains class names like 'box1', 'box2', ...)
+  const arr = seq.map((val) => val.replace("box", ""));
+  // Check if all boxNumbers are present in arr
+  const allPresent = boxNumbers.every((num) => arr.includes(num.toString()));
+  return allPresent;
+};
+
+const boxClickHandler = (e) => {
+  e.preventDefault();
+  e.currentTarget.style.backgroundColor = "red";
+  seq.push(e.currentTarget.classList[0]);
+  if (checkAllBoxesAreRed()) {
+    const boxesArr = [...boxes];
+    const arr = seq.slice().reverse(); // Reverse the sequence
+    arr.forEach((boxclass) => {
+      const num = boxclass.replace("box", "");
+      setTimeout(() => {
+        boxesArr[num - 1].style.backgroundColor = "green";
+      }, 1000 * (arr.indexOf(boxclass) + 1));
+    });
+    Array.from(boxes).forEach((box) => {
+      box.style.backgroundColor = "gray";
+    });
+  }
+};
+
+Array.from(boxes).forEach((box) => {
+  box.addEventListener("click", boxClickHandler);
+});
